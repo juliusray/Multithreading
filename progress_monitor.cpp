f0d70f06c5fa993b1204cd7c75a130dfaf6190b1
juliusray <juliusray.inocencio@gmail.com>
2015-05-27 14:14:55 -0700

    added progress_monitor class

---
+++ progress_monitor.cpp
@@ -0,0 +1,30 @@
+// Implementation file for progress_monitor.cpp
+
+#include "progress_monitor.h"
+
+Progress_Monitor::Progress_Monitor() {
+	num_printed = 0;
+}
+
+// Internal thread function for pthread library
+// calculates how much progress
+// the word counter has done and prints a progress
+// bar to indicate where it is in completion
+// example: 
+// ---------+----------+---------+
+// every '+' signifies 20% completion
+
+void *display_progress(void *prog_status) {
+	PROGRESS_STATUS* progress = (PROGRESS_STATUS*) prog_status;
+	int num_printed = 0;
+	
+	while (num_printed < NUM_HYPHENS) {
+		int processed = ((float) progress->current_status / (float) progress->termination_val * NUM_HYPHENS) - num_printed;
+		num_printed += processed;
+	
+		for (int i = 0; i < processed; i++) 
+			(num_printed % PROGRESS_WIDTH) == 0 ? cout << "+" : cout << "-";	
+		cout.flush();
+		}
+	pthread_exit(NULL);
+}
---
+++ progress_monitor.h
@@ -0,0 +1,29 @@
+// Header file for progress_monitor.h
+
+#ifndef progress_monitor_h
+#define progress_monitor_h
+
+#include "progress_status.h"
+
+#include <iostream>
+#include <string>
+#include <pthread.h>
+
+using namespace std;
+const int NUM_HYPHENS = 50;
+const int PROGRESS_WIDTH = 10;
+
+
+//thread function
+void *display_progress(void *prog_status);
+
+class Progress_Monitor {
+private: 
+	int num_printed;
+	PROGRESS_STATUS* progress;
+public:
+	Progress_Monitor();
+
+};
+
+#endif
